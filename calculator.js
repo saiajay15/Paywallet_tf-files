8b7485b2590c82e5fb7218ac1982d50cef78367d
ajay <saiao@maveric-system.com>
2021-08-16 00:12:15 +0530

    added division functionality

--- calculator.js
+++ calculator.js
@@ -9,6 +9,12 @@ function subtract() {
     document.getElementById("result").value = Number(num1) - Number(num2);
 }
 
+function divide() {
+    var num1 = document.getElementById("firstNumber").value;
+    var num2 = document.getElementById("secondNumber").value;
+    document.getElementById("result").value = Number(num1) / Number(num2);
+}
+
 function multiply() {
     var num1 = document.getElementById("firstNumber").value;
     var num2 = document.getElementById("secondNumber").value;
